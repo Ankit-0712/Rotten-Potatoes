0266e4fab64b6a270e9871823c89617de78a6f3b
Ankit Pant <159069425+Ankit-0712@users.noreply.github.com>
2025-11-29 13:45:20 +0530

    final commit

--- src/app/movie/movie.ts
+++ src/app/movie/movie.ts
@@ -1,48 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Header } from "../header/header";
-import { ActivatedRoute, Route } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { FeatureModule } from "../feature/feature-module";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movie',
-  imports: [Header],
+  standalone: true,
+  imports: [Header, FeatureModule, CommonModule],
   templateUrl: './movie.html',
-  styleUrl: './movie.scss'
+  styleUrls: ['./movie.scss']
 })
-export class Movie implements OnInit{
+export class Movie implements OnInit {
   type = '';
   id = '';
-  url= '';
-  movies : any;
-  movie : any;
+  url = '';
+  movies: any;
+  movie: any;
 
-  constructor(private route : ActivatedRoute, private http : HttpClient){}
-
+  constructor(private route: ActivatedRoute, private http: HttpClient, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
-      this.type = this.route.snapshot.params['type'];
-      this.id = this.route.snapshot.params['id'];
-      if(this.type === 'trending'){
-        this.url = 'http://localhost:4200//assets/data/trending-movies.json';
-      }
-      if(this.type === 'theatre'){
-        this.url = 'http://localhost:4200//assets/data/theatre-movies.json';
-      }
-      if(this.type === 'popular'){
-        this.url = 'http://localhost:4200//assets/data/popular-movies.json';
-      }
-      this.getMovie();
+    this.type = this.route.snapshot.params['type'];
+    this.id = this.route.snapshot.params['id'];
 
+    if (this.type === 'trending') {
+      this.url = '/assets/data/trending-movies.json';
+    }
+    if (this.type === 'theatre') {
+      this.url = '/assets/data/theatre-movies.json';
+    }
+    if (this.type === 'popular') {
+      this.url = '/assets/data/popular-movies.json';
+    }
+
+    this.getMovie();
   }
 
-  getMovie(){
-    this.http.get(this.url).subscribe((movies)=>{
+  getMovie() {
+    this.http.get<any[]>(this.url).subscribe((movies: any[]) => {
       this.movies = movies;
-      let index =this.movies.findIndex((movie: { id: string; }) => movie.id == this.id);
-      if(index > -1){
+      let index = this.movies.findIndex((movie: { id: string; }) => movie.id == this.id);
+
+      if (index > -1) {
         this.movie = this.movies[index];
-        
       }
-    })
+      this.cdr.detectChanges();
+    });
   }
 }
+
